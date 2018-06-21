70575c6231b6d4352a20ea6e463d73bf8ef0b951
ghstahl@gmail.com <ghstahl@gmail.com>
2018-06-21 07:41:47 -0700

    new tests

---
+++ src/IdentityServer4.LiteDB.Tests/Mappers/IdentityResourceMapperProfileTests.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer4.LiteDB.Mappers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IdentityServer4.LiteDB.Tests.Mappers
+{
+    [TestClass]
+    public class IdentityResourceMapperProfileTests
+    {
+        [TestMethod]
+        public void Map()
+        {
+            var model = new Models.IdentityResource();
+            var entity = IdentityResourceMappers.ToEntity(model);
+            model = IdentityResourceMappers.ToModel(entity);
+
+            // Assert
+            Assert.IsNotNull(entity);
+            Assert.IsNotNull(model);
+        }
+    }
+    [TestClass]
+    public class PersistedGrantMapperProfileTests
+    {
+        [TestMethod]
+        public void Map()
+        {
+            var model = new Models.PersistedGrant();
+            var entity = PersistedGrantMappers.ToEntity(model);
+            model = PersistedGrantMappers.ToModel(entity);
+
+            // Assert
+            Assert.IsNotNull(entity);
+            Assert.IsNotNull(model);
+        }
+    }
+}
---
+++ src/IdentityServer4.LiteDB.Tests/Mappers/PersistedGrantMapperProfileTests.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.LiteDB.Mappers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IdentityServer4.LiteDB.Tests.Mappers
+{
+    [TestClass]
+    public class PersistedGrantMapperProfileTests
+    {
+        [TestMethod]
+        public void Map()
+        {
+            var model = new Models.PersistedGrant();
+            var entity = PersistedGrantMappers.ToEntity(model);
+            model = PersistedGrantMappers.ToModel(entity);
+
+            // Assert
+            Assert.IsNotNull(entity);
+            Assert.IsNotNull(model);
+        }
+    }
+}
